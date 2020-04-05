917de84453d61f877446b2c18bc585a792247f02
Will Halstead <will.r.halstead@gmail.com>
2020-04-05 12:24:56 -0400

    refactoring for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: "heroku_11d5f2b7d65bb46"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
